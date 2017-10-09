075d5234c43fefbdaf73b1177877aa2577eabff2
Bharat Srirangam <Bharat_Srirangam@Bharats-MacBook-Pro.local>
2017-10-08 22:39:30 -0400

    Preliminary Website skeleton

---
+++ js/archive.js
@@ -0,0 +1,27 @@
+var courseTree = document.getElementById("courseTreeInfo");
+var medicheck = document.getElementById("medicheckInfo");
+var projectEuler = document.getElementById("projectEulerInfo");
+var boRats = document.getElementById("boRatsIncInfo");
+var civilization = document.getElementById("civilizationInfo");
+
+var array = [courseTree, medicheck, projectEuler, boRats, civilization];
+
+//As More project descriptions are added to the html page, add them here above in the list and in the array.
+
+for (var i = array.length - 1; i >= 0; i--) {
+	array[i].addEventListener('click', toReplace);
+}
+
+function toReplace() {
+	var allBoxes = document.getElementsByClassName("boxes");
+	console.log(allBoxes);
+	for (var i = allBoxes.length - 1; i >= 0; i--) {
+		if (!allBoxes[i].classList.contains("hiding")) {
+			allBoxes[i].classList.add("hiding");
+		}
+	}
+	var onClick = this.getAttribute("id") + "Box";
+
+	document.getElementById(onClick).classList.remove("hiding");
+}
+
