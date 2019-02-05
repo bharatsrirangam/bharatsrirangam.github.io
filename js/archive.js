cd8bdcb8005ec3fca0b55c1b29061f67ca744d54
Bharat Srirangam <Bharat_Srirangam@Bharats-MacBook-Pro.local>
2019-02-05 07:55:40 -0500

    Updated Resume/Website for Spring Semester

--- js/archive.js
+++ js/archive.js
@@ -5,10 +5,11 @@ var boRats = document.getElementById("boRatsIncInfo");
 var civilization = document.getElementById("civilizationInfo");
 var neuralNetwork = document.getElementById("neuralNetworkInfo");
 var einsteins = document.getElementById("einsteinsInfo");
+var locationScores = document.getElementById("locationScoresInfo")
 
-var array = [courseTree, medicheck, projectEuler, boRats, civilization, neuralNetwork, einsteins];
+var array = [courseTree, medicheck, projectEuler, boRats, civilization, neuralNetwork, einsteins, locationScores];
 
-//As More project descriptions are added to the html page, add them here above in the list and in the array.
+//As More project descriptions are added to the html page, add them here above in the list and in the array. Also add to the html pages menu section at the top
 
 for (var i = array.length - 1; i >= 0; i--) {
 	array[i].addEventListener('click', toReplace);
